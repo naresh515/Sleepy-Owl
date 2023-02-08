e6ad1a789bd77b50478b175d6d1c84652db3405e
Naresh Tak <taknaresh515@gmail.com>
2023-02-08 12:27:43 +0530

    done some changes

--- style.js
+++ style.js
@@ -98,4 +98,20 @@ $(document).ready(function () {
         $(this).parent(".accoridan").find(".main").slideToggle();
         $(this).parent(".accoridan").find(".accordin-item").slideDown();
     });
+
+    //cart
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 0 ? 0 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
 })
